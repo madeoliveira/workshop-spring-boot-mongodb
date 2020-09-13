bcb38ca63ae19e481268bcc557e1d141131cceb2
Marcio Oliveira <marcio.alves78@hotmail.com>
2020-09-12 21:47:06 -0300

    Referencing user posts

--- src/main/java/com/training/workshopmongo/config/instantiation.java
+++ src/main/java/com/training/workshopmongo/config/instantiation.java
@@ -44,6 +44,9 @@ public class instantiation implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
 	}
 	
 }
