e0bdfbfa8b1e1cf75d9d11c38c5a8a7a83129f09
Marcio Oliveira <marcio.alves78@hotmail.com>
2020-09-13 12:07:39 -0300

    Add comments to posts

--- src/main/java/com/training/workshopmongo/config/instantiation.java
+++ src/main/java/com/training/workshopmongo/config/instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.training.workshopmongo.domain.Post;
 import com.training.workshopmongo.domain.User;
 import com.training.workshopmongo.dto.AuthorDTO;
+import com.training.workshopmongo.dto.CommentDTO;
 import com.training.workshopmongo.repository.PostRepository;
 import com.training.workshopmongo.repository.UserRepository;
 
@@ -41,6 +42,12 @@ public class instantiation implements CommandLineRunner {
 	    Post post1 = new Post(null, sdf.parse("21/03/2018"), "Start the jorney","I will travel to São Paulo", new AuthorDTO(maria));
 	    Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good Morning","I woke up happy today!",new AuthorDTO(maria));
 	    
+	    CommentDTO c1 = new CommentDTO("Good trip brother", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Enjoy", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Have a great day",sdf.parse("23/03/2018"),new AuthorDTO(alex));
+	    
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
--- src/main/java/com/training/workshopmongo/domain/Post.java
+++ src/main/java/com/training/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.training.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.training.workshopmongo.dto.AuthorDTO;
+import com.training.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,8 +21,12 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 	}
+	
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
@@ -58,6 +65,16 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -81,9 +98,7 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
+
+
 	
 }
